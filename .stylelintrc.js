956af504bb5a1d5bb12875b3fdeeb4e291a51156
liangsky <640634387@qq.com>
2023-04-11 22:38:49 +0800

    chore: remove stylelint

--- .stylelintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: require.resolve('@umijs/lint/dist/config/stylelint'),
-  rules: {
-    // your rules
-  },
-};
--- script/lint.js
+++ script/lint.js
@@ -2,5 +2,9 @@ const lint = require('@umijs/lint/dist/index').default;
 
 lint(
   { cwd: process.cwd() },
-  { _: ['packages/**/{src,test}/**/*.{js,jsx,ts,tsx,less,css}'], fix: true },
+  {
+    _: ['packages/**/{src,test}/**/*.{js,jsx,ts,tsx}'],
+    fix: true,
+    eslintOnly: true,
+  },
 );
