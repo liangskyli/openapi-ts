f3901ebd7a7607030865ee5fc1169f8662752990
liangsky <640634387@qq.com>
2023-04-26 22:39:18 +0800

    refactor: genInterfaceFile code refactor

--- packages/openapi-gen-ts/src/gen/gen-interface-file.ts
+++ packages/openapi-gen-ts/src/gen/gen-interface-file.ts
@@ -50,6 +50,232 @@ const getMethod = (itemValue?: OpenapiDefinition) => {
   return method;
 };
 
+const processMethodMediaData = (
+  itemValue: OpenapiDefinition['properties'],
+  method: string,
+) => {
+  const haveQuery =
+    !!itemValue![method]?.properties?.parameters?.properties?.query?.properties;
+
+  let responseMediaType = '';
+  const methodObjRequired = itemValue![method]?.required ?? [];
+  const responsesProperties =
+    itemValue![method]?.properties?.responses?.properties;
+  let responseCode: '200' | 'default' = '200';
+  if (!responsesProperties?.['200']) {
+    responseCode = 'default';
+  }
+  // first use responses 200, then use responses default
+  const responsesContentProperties =
+    responsesProperties?.[responseCode]?.properties?.content?.properties;
+  if (responsesContentProperties) {
+    // responses content properties only use first key
+    responseMediaType = Object.keys(responsesContentProperties)[0];
+  }
+
+  let haveBody = false;
+  let bodyMediaTypes = [''];
+  const bodyContentProperties =
+    itemValue![method]?.properties?.requestBody?.properties?.content
+      ?.properties;
+  if (bodyContentProperties) {
+    bodyMediaTypes = Object.keys(bodyContentProperties);
+    haveBody = true;
+  }
+  return {
+    haveQuery,
+    haveBody,
+    bodyMediaTypes,
+    responseMediaType,
+    methodObjRequired,
+    responsesContentProperties,
+    responseCode,
+    method,
+  };
+};
+
+type IReturnTypeProcessMethodMediaData = ReturnType<
+  typeof processMethodMediaData
+>;
+type IGenInterfaceAPITypeBodyOpts = IReturnTypeProcessMethodMediaData &
+  Pick<Required<IOpts>, 'requestQueryOmit' | 'requestBodyOmit'> & {
+    url: string;
+  };
+
+const genInterfaceAPIType = {
+  head: (interfaceAPIType: string[]) => {
+    interfaceAPIType.push(`${fileTip}
+  import type { paths } from './ts-schema';
+  `);
+    interfaceAPIType.push('\n export interface IApi {');
+  },
+  body: (interfaceAPIType: string[], opts: IGenInterfaceAPITypeBodyOpts) => {
+    const {
+      haveQuery,
+      haveBody,
+      bodyMediaTypes,
+      responseMediaType,
+      methodObjRequired,
+      responsesContentProperties,
+      responseCode,
+      method,
+      requestQueryOmit,
+      requestBodyOmit,
+      url,
+    } = opts;
+    interfaceAPIType.push(`'${url}': {`);
+    if (haveQuery) {
+      const omitKeys = requestQueryOmit
+        .map((omitItem) => `'${omitItem}'`)
+        .join(' | ');
+      const queryInterface = `paths['${url}']['${method}']['parameters']['query']`;
+      interfaceAPIType.push(
+        omitKeys
+          ? `Query: Omit<${queryInterface}, ${omitKeys}>;`
+          : `Query: ${queryInterface};`,
+      );
+    }
+    if (haveBody) {
+      const omitKeys = requestBodyOmit
+        .map((omitItem) => `'${omitItem}'`)
+        .join(' | ');
+      const requestBodyRequired =
+        methodObjRequired.find((item) => item === 'requestBody') !== undefined;
+      const bodyInterfaces = bodyMediaTypes.map((bodyMediaType) => {
+        let bodyInterface = `paths['${url}']['${method}']['requestBody']['content']['${bodyMediaType}']`;
+        if (!requestBodyRequired) {
+          bodyInterface = `(paths['${url}']['${method}']['requestBody'] & Record<string, never>)['content']['${bodyMediaType}']`;
+        }
+        return omitKeys ? `Omit<${bodyInterface}, ${omitKeys}>` : bodyInterface;
+      });
+      interfaceAPIType.push(
+        `Body${requestBodyRequired ? '' : '?'}: ${bodyInterfaces.join(' & ')};`,
+      );
+    }
+    if (responsesContentProperties) {
+      interfaceAPIType.push(
+        `Response: paths['${url}']['${method}']['responses']['${responseCode}']['content']['${responseMediaType}'];`,
+      );
+    } else {
+      interfaceAPIType.push('Response: any');
+    }
+    interfaceAPIType.push('};');
+  },
+  footer: (interfaceAPIType: string[]) => {
+    interfaceAPIType.push('}');
+  },
+};
+
+type IGenRequestAPIHeadOpts = Partial<
+  Pick<Required<IOpts>['requestFile'], 'requestParamsType' | 'path'>
+>;
+type IGenRequestAPIBodyOpts = IReturnTypeProcessMethodMediaData &
+  Pick<Required<IOpts>['requestFile'], 'requestParamsType'> & {
+    url: string;
+  };
+
+const genRequestAPI = {
+  head: (requestAPI: string[], opts: IGenRequestAPIHeadOpts) => {
+    const { requestParamsType, path: requestFilePath } = opts;
+    requestAPI.push(`${fileTip}
+  ${
+    requestParamsType !== ''
+      ? `import type {${requestParamsType} } from '${
+          requestFilePath ? requestFilePath : './request'
+        }';`
+      : ''
+  } 
+   import request from '${requestFilePath ? requestFilePath : './request'}';
+   import type { IApi } from './interface-api';
+  `);
+    requestAPI.push('\n');
+    requestAPI.push(`
+    type IConfig<T extends Record<any, any>, U extends Record<any, any>> = T & U;
+   `);
+    requestAPI.push('\n export const requestApi = {');
+  },
+  body: (requestAPI: string[], opts: IGenRequestAPIBodyOpts) => {
+    const {
+      haveQuery,
+      haveBody,
+      methodObjRequired,
+      method,
+      requestParamsType,
+      url,
+    } = opts;
+    const IConfigT: string[] = [];
+    if (haveQuery || haveBody) {
+      IConfigT.push('Omit<T');
+      if (requestParamsType !== '') {
+        IConfigT.push(` & ${requestParamsType}, 'method' | 'url'`);
+      } else {
+        IConfigT.push(', ');
+      }
+      if (haveQuery) {
+        if (requestParamsType !== '') {
+          IConfigT.push('| "params"');
+        } else {
+          IConfigT.push('"params"');
+        }
+      }
+      if (haveBody) {
+        if (requestParamsType !== '') {
+          IConfigT.push('| "data"');
+        } else {
+          if (haveQuery) {
+            IConfigT.push(' | ');
+          }
+          IConfigT.push('"data"');
+        }
+      }
+      IConfigT.push('>,');
+    } else {
+      if (requestParamsType !== '') {
+        IConfigT.push(`Omit<T & ${requestParamsType}, 'method' | 'url'>,`);
+      } else {
+        IConfigT.push('T,');
+      }
+    }
+
+    requestAPI.push(`'${url}': <T extends Record<any, any> = Record<string, never>>(
+        config: IConfig<
+          ${IConfigT.length > 0 ? IConfigT.join('') : 'T,'}
+      `);
+    requestAPI.push(!haveQuery && !haveBody ? 'Record<string, never>' : '{');
+    if (haveQuery) {
+      requestAPI.push(`params: IApi['${url}']['Query'];`);
+    }
+    if (haveBody) {
+      const requestBodyRequired =
+        methodObjRequired.find((item) => item === 'requestBody') !== undefined;
+      requestAPI.push(
+        `data${requestBodyRequired ? '' : '?'}: IApi['${url}']['Body'];`,
+      );
+    }
+    if (haveQuery || haveBody) {
+      requestAPI.push('}');
+    }
+    requestAPI.push(`>,
+      ): Promise<IApi['${url}']['Response']> => {  
+      const { ${haveQuery ? 'params,' : ''} ${
+      haveBody ? 'data,' : ''
+    } ...otherConfig } = config;
+
+      return request({
+        method: '${method}',
+        url: '${url}',
+        ${haveQuery ? 'params: params,' : ''}
+        ${haveBody ? 'data: data,' : ''}
+        ...otherConfig,
+      });
+    },
+      `);
+  },
+  footer: (requestAPI: string[]) => {
+    requestAPI.push('}');
+  },
+};
+
 const genInterfaceFile = async (opts: IOpts) => {
   const {
     genSchemaAPIAbsolutePath,
@@ -67,28 +293,11 @@ const genInterfaceFile = async (opts: IOpts) => {
   }
 
   const interfaceAPIType: string[] = [];
-  interfaceAPIType.push(`${fileTip}
-  import type { paths } from './ts-schema';
-  `);
-  interfaceAPIType.push('\n export interface IApi {');
+  genInterfaceAPIType.head(interfaceAPIType);
 
   const requestAPI: string[] = [];
-  requestAPI.push(`${fileTip}
-  ${
-    requestParamsType !== ''
-      ? `import type {${requestParamsType} } from '${
-          requestFilePath ? requestFilePath : './request'
-        }';`
-      : ''
-  } 
-   import request from '${requestFilePath ? requestFilePath : './request'}';
-   import type { IApi } from './interface-api';
-  `);
-  requestAPI.push('\n');
-  requestAPI.push(`
-    type IConfig<T extends Record<any, any>, U extends Record<any, any>> = T & U;
-   `);
-  requestAPI.push('\n export const requestApi = {');
+  genRequestAPI.head(requestAPI, { requestParamsType, path: requestFilePath });
+
   const schemaData = fs.readJSONSync(
     path.join(genSchemaAPIAbsolutePath, 'schema.json'),
   ) as OpenapiDefinition;
@@ -97,146 +306,24 @@ const genInterfaceFile = async (opts: IOpts) => {
     Object.entries(schemaPathData).forEach(([url, urlValue]) => {
       const itemValue = urlValue.properties;
       const method = getMethod(itemValue);
-      let haveQuery = false;
-      let haveBody = false;
-      let bodyMediaTypes = [''];
-      let responseMediaType = '';
       if (method) {
-        const methodObjRequired = itemValue![method]?.required ?? [];
-        const responsesProperties =
-          itemValue![method]?.properties?.responses?.properties;
-        let responseCode: '200' | 'default' = '200';
-        if (!responsesProperties?.['200']) {
-          responseCode = 'default';
-        }
-        // first use responses 200, then use responses default
-        const responsesContentProperties =
-          responsesProperties?.[responseCode]?.properties?.content?.properties;
-        if (responsesContentProperties) {
-          // responses content properties only use first key
-          responseMediaType = Object.keys(responsesContentProperties)[0];
-        }
-        haveQuery =
-          !!itemValue![method]?.properties?.parameters?.properties?.query
-            ?.properties;
-        const bodyContentProperties =
-          itemValue![method]?.properties?.requestBody?.properties?.content
-            ?.properties;
-        if (bodyContentProperties) {
-          bodyMediaTypes = Object.keys(bodyContentProperties);
-          haveBody = true;
-        }
-
-        const IConfigT: string[] = [];
-        if (haveQuery || haveBody) {
-          IConfigT.push('Omit<T');
-          if (requestParamsType !== '') {
-            IConfigT.push(` & ${requestParamsType}, 'method' | 'url'`);
-          } else {
-            IConfigT.push(', ');
-          }
-          if (haveQuery) {
-            if (requestParamsType !== '') {
-              IConfigT.push('| "params"');
-            } else {
-              IConfigT.push('"params"');
-            }
-          }
-          if (haveBody) {
-            if (requestParamsType !== '') {
-              IConfigT.push('| "data"');
-            } else {
-              if (haveQuery) {
-                IConfigT.push(' | ');
-              }
-              IConfigT.push('"data"');
-            }
-          }
-          IConfigT.push('>,');
-        } else {
-          if (requestParamsType !== '') {
-            IConfigT.push(`Omit<T & ${requestParamsType}, 'method' | 'url'>,`);
-          } else {
-            IConfigT.push('T,');
-          }
-        }
-
-        interfaceAPIType.push(`'${url}': {`);
-        requestAPI.push(`'${url}': <T extends Record<any, any> = Record<string, never>>(
-        config: IConfig<
-          ${IConfigT.length > 0 ? IConfigT.join('') : 'T,'}
-      `);
-        requestAPI.push(
-          !haveQuery && !haveBody ? 'Record<string, never>' : '{',
-        );
-        if (haveQuery) {
-          const omitKeys = requestQueryOmit
-            .map((omitItem) => `'${omitItem}'`)
-            .join(' | ');
-          const queryInterface = `paths['${url}']['${method}']['parameters']['query']`;
-          interfaceAPIType.push(
-            omitKeys
-              ? `Query: Omit<${queryInterface}, ${omitKeys}>;`
-              : `Query: ${queryInterface};`,
-          );
-          requestAPI.push(`params: IApi['${url}']['Query'];`);
-        }
-        if (haveBody) {
-          const omitKeys = requestBodyOmit
-            .map((omitItem) => `'${omitItem}'`)
-            .join(' | ');
-          const requestBodyRequired =
-            methodObjRequired.find((item) => item === 'requestBody') !==
-            undefined;
-          const bodyInterfaces = bodyMediaTypes.map((bodyMediaType) => {
-            let bodyInterface = `paths['${url}']['${method}']['requestBody']['content']['${bodyMediaType}']`;
-            if (!requestBodyRequired) {
-              bodyInterface = `(paths['${url}']['${method}']['requestBody'] & Record<string, never>)['content']['${bodyMediaType}']`;
-            }
-            return omitKeys
-              ? `Omit<${bodyInterface}, ${omitKeys}>`
-              : bodyInterface;
-          });
-          interfaceAPIType.push(
-            `Body${requestBodyRequired ? '' : '?'}: ${bodyInterfaces.join(
-              ' & ',
-            )};`,
-          );
-          requestAPI.push(
-            `data${requestBodyRequired ? '' : '?'}: IApi['${url}']['Body'];`,
-          );
-        }
-        if (responsesContentProperties) {
-          interfaceAPIType.push(
-            `Response: paths['${url}']['${method}']['responses']['${responseCode}']['content']['${responseMediaType}'];`,
-          );
-        } else {
-          interfaceAPIType.push('Response: any');
-        }
-        interfaceAPIType.push('};');
-        if (haveQuery || haveBody) {
-          requestAPI.push('}');
-        }
-        requestAPI.push(`>,
-      ): Promise<IApi['${url}']['Response']> => {  
-      const { ${haveQuery ? 'params,' : ''} ${
-          haveBody ? 'data,' : ''
-        } ...otherConfig } = config;
-
-      return request({
-        method: '${method}',
-        url: '${url}',
-        ${haveQuery ? 'params: params,' : ''}
-        ${haveBody ? 'data: data,' : ''}
-        ...otherConfig,
-      });
-    },
-      `);
+        const methodMediaData = processMethodMediaData(itemValue, method);
+        genInterfaceAPIType.body(interfaceAPIType, {
+          ...methodMediaData,
+          requestQueryOmit,
+          requestBodyOmit,
+          url,
+        });
+        genRequestAPI.body(requestAPI, {
+          ...methodMediaData,
+          requestParamsType,
+          url,
+        });
       }
     });
   }
-  interfaceAPIType.push('}');
-  requestAPI.push('}');
+  genInterfaceAPIType.footer(interfaceAPIType);
+  genRequestAPI.footer(requestAPI);
 
   const interfaceAPITypeAbsolutePath = path.join(
     genSchemaAPIAbsolutePath,
