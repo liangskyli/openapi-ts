22b9f82abb077a8295f031eb906f055564610a02
liangsky <640634387@qq.com>
2023-05-26 11:00:08 +0800

    feat: upgrade openapi-typescript to fix js-yaml $refs generate ts bug and add unit test

--- packages/openapi-gen-ts/test/cli/request.config.ts
+++ packages/openapi-gen-ts/test/cli/request.config.ts
@@ -2,8 +2,12 @@ import type { IGenTsDataOptsCLI } from '../../lib';
 
 const config: IGenTsDataOptsCLI = [
   {
-    genTsDir: './test/all-gen-dirs/gen-ts-dir-cli',
+    genTsDir: './test/all-gen-dirs/gen-ts-dir-cli1',
     openapiPath: './test/example/openapi/openapiv3-example.json',
+  },
+  {
+    genTsDir: './test/all-gen-dirs/gen-ts-dir-cli2',
+    openapiPath: './test/example/openapi/openapi-v3.yaml',
   },
 ];
 export default config;
--- packages/openapi-gen-ts/test/gen/file/gen-ts-schema.test.ts
+++ packages/openapi-gen-ts/test/gen/file/gen-ts-schema.test.ts
@@ -1,26 +1,51 @@
 import { getAbsolutePath, winPath } from '@liangskyli/utils';
 import fs from 'fs-extra';
-import { expect, test, vi } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { GenTsSchema } from '../../../src/gen/file/gen-ts-schema';
 
-test('Generate schema-api/ts-schema.ts file', async () => {
-  const schema = getAbsolutePath(
-    './test/example/openapi/openapiv3-example.json',
-  );
+describe('Generate schema-api/ts-schema.ts file', () => {
+  test('json file', async () => {
+    const schema = getAbsolutePath(
+      './test/example/openapi/openapiv3-example.json',
+    );
 
-  const genTsSchema = new GenTsSchema(schema, {
-    genTsAbsolutePath: '/',
-    openAPITSOptions: {},
+    const genTsSchema = new GenTsSchema(schema, {
+      genTsAbsolutePath: '/',
+      openAPITSOptions: {},
+    });
+    await genTsSchema.generator();
+    genTsSchema.writeFile();
+    const args = vi.mocked(fs.writeFileSync).mock.calls[0];
+    expect(
+      (global as any).writePrettierFileArgs.prettierOptions,
+    ).toBeUndefined();
+    expect(winPath(args[0] as string)).toBe('/ts-schema.ts');
+    expect((global as any).writePrettierFileArgs.successTip).toBe(
+      'Generate schema-api/ts-schema.ts success',
+    );
+    await expect(args[1]).toMatchFileSnapshot(
+      './__test__snapshots__/ts-schema-json.ts',
+    );
   });
-  await genTsSchema.generator();
-  genTsSchema.writeFile();
-  const args = vi.mocked(fs.writeFileSync).mock.calls[0];
-  expect((global as any).writePrettierFileArgs.prettierOptions).toBeUndefined();
-  expect(winPath(args[0] as string)).toBe('/ts-schema.ts');
-  expect((global as any).writePrettierFileArgs.successTip).toBe(
-    'Generate schema-api/ts-schema.ts success',
-  );
-  await expect(args[1]).toMatchFileSnapshot(
-    './__test__snapshots__/ts-schema.ts',
-  );
+  test('yaml file', async () => {
+    const schema = getAbsolutePath('./test/example/openapi/openapi-v3.yaml');
+
+    const genTsSchema = new GenTsSchema(schema, {
+      genTsAbsolutePath: '/',
+      openAPITSOptions: {},
+    });
+    await genTsSchema.generator();
+    genTsSchema.writeFile();
+    const args = vi.mocked(fs.writeFileSync).mock.calls[0];
+    expect(
+      (global as any).writePrettierFileArgs.prettierOptions,
+    ).toBeUndefined();
+    expect(winPath(args[0] as string)).toBe('/ts-schema.ts');
+    expect((global as any).writePrettierFileArgs.successTip).toBe(
+      'Generate schema-api/ts-schema.ts success',
+    );
+    await expect(args[1]).toMatchFileSnapshot(
+      './__test__snapshots__/ts-schema-yaml.ts',
+    );
+  });
 });
