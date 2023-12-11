d5bf8c4c42d7d03a6f60aa7bd42d608b3f64e7b0
liangsky <640634387@qq.com>
2023-12-11 14:31:43 +0800

    chore: upgrade dependencies

--- packages/openapi-gen-ts/vitest.config.ts
+++ packages/openapi-gen-ts/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
-    include: ['./**/*.test.{ts,js}'],
+    include: ['**/*.test.{ts,js}'],
     environment: 'node',
     setupFiles: ['./vitest.setup.ts'],
     testTimeout: 1000 * 60,
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,js}'],
+    },
   },
 });
